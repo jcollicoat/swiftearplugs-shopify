61c743226d74334bf452b5c99727447ee39c58ea
Joseph Collicoat <jcollicoat@gmail.com>
2024-08-07 16:47:44 +0200

    hero only homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
-// import { GeistSans } from 'geist/font/sans';
+/* eslint-disable @next/next/no-page-custom-font */
 import { cookies } from 'next/headers';
 import { ReactNode } from 'react';
-import { Toaster } from 'sonner';
+// import { Toaster } from 'sonner';
 import { CartProvider } from 'components/template/cart/cart-context';
-import { Navbar } from 'components/template/layout/navbar';
-import { WelcomeToast } from 'components/template/welcome-toast';
+// import { Navbar } from 'components/template/layout/navbar';
+// import { WelcomeToast } from 'components/template/welcome-toast';
 import { getCart } from 'lib/shopify';
 import { ensureStartsWith } from 'lib/utils';
-import './globals.css';
 import 'styles/layout.scss';
 
 const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
@@ -53,13 +52,26 @@ export default async function RootLayout({
 
     return (
         <html lang="en">
-            <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+            <head>
+                <link rel="icon" href="../public/favicon.ico" key="" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
+            <body>
                 <CartProvider cartPromise={cart}>
-                    <Navbar />
+                    {/* <Navbar /> */}
                     <main>
                         {children}
-                        <Toaster closeButton />
-                        <WelcomeToast />
+                        {/* <Toaster closeButton /> */}
+                        {/* <WelcomeToast /> */}
                     </main>
                 </CartProvider>
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { Hero } from 'components/layouts/Hero/Hero';
-import { Carousel } from 'components/template/carousel';
-import { ThreeItemGrid } from 'components/template/grid/three-items';
-import Footer from 'components/template/layout/footer';
+// import { Carousel } from 'components/template/carousel';
+// import { ThreeItemGrid } from 'components/template/grid/three-items';
+// import Footer from 'components/template/layout/footer';
 
 export const metadata = {
-    description:
-        'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+    description: 'Swift Earplugs bring you closer to the music you love.',
     openGraph: {
         type: 'website',
     },
@@ -15,9 +14,9 @@ export default function HomePage() {
     return (
         <>
             <Hero />
-            <ThreeItemGrid />
-            <Carousel />
-            <Footer />
+            {/* <ThreeItemGrid /> */}
+            {/* <Carousel /> */}
+            {/* <Footer /> */}
         </>
     );
 }

ead12a904ba9137f47ab5f5aed0302f12a2d2245
Joseph Collicoat <jcollicoat@gmail.com>
2024-08-19 12:24:57 +0100

    hero image blur

--- components/layouts/Hero/Hero.tsx
+++ components/layouts/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { FC } from 'react';
 import { Logo } from 'components/generics/Logo/Logo';
+import imageBlur from 'public/swift-hero-1-blur.jpg';
 import image from 'public/swift-hero-1.jpg';
 import styles from './Hero.module.scss';
 
@@ -9,7 +10,13 @@ export const Hero: FC = () => {
         <div className={styles.wrapper}>
             <Logo />
             <div className={styles.image}>
-                <Image src={image} placeholder="empty" alt="" fill />
+                <Image
+                    src={image.src}
+                    blurDataURL={imageBlur.src}
+                    placeholder="blur"
+                    alt=""
+                    fill
+                />
             </div>
             <div className={styles.content}>
                 <h1>Dance, laugh, and live loudly.</h1>

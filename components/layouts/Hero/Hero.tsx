d6bc75d9d2782920773b08e46f0895f00af0eb73
Joseph Collicoat <jcollicoat@gmail.com>
2024-08-19 12:14:24 +0100

    hero design

--- components/layouts/Hero/Hero.tsx
+++ components/layouts/Hero/Hero.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
 import { FC } from 'react';
 import { Logo } from 'components/generics/Logo/Logo';
+import image from 'public/swift-hero-1.jpg';
 import styles from './Hero.module.scss';
 
 export const Hero: FC = () => {
     return (
         <div className={styles.wrapper}>
             <Logo />
+            <div className={styles.image}>
+                <Image src={image} placeholder="empty" alt="" fill />
+            </div>
             <div className={styles.content}>
                 <h1>Dance, laugh, and live loudly.</h1>
                 <h2>Swift&nbsp;Earplugs have you covered.</h2>

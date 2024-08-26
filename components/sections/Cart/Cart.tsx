1daf7f1d7f85c4e65c4636f979b41b07fd72cba8
Joseph Collicoat <jcollicoat@gmail.com>
2024-08-26 17:36:35 +0100

    hide cart until product added

--- components/sections/Cart/Cart.tsx
+++ components/sections/Cart/Cart.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { createCartAndSetCookie } from 'components/template/cart/actions';
 import { useCart } from 'components/template/cart/cart-context';
 import styles from './Cart.module.scss';
@@ -9,6 +9,7 @@ import { CartSummary } from './components/Cart.Summary';
 
 export const Cart: FC = () => {
     const { cart } = useCart();
+    const cartInteracted = useRef(false);
     const [isOpen, setIsOpen] = useState(false);
     const toggleCart = useCallback(() => setIsOpen(!isOpen), [isOpen]);
 
@@ -20,9 +21,13 @@ export const Cart: FC = () => {
         if (cart?.totalQuantity === 0) {
             setIsOpen(false);
         }
+
+        if ((cart?.totalQuantity ?? 0) > 0) {
+            cartInteracted.current = true;
+        }
     }, [cart]);
 
-    if (!cart) {
+    if (!cart || !cartInteracted.current) {
         return null;
     }
 

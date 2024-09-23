8c0713287a818959e70158f03b6bc723e8c14d62
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-23 12:35:13 +0100

    header scroll to hook

--- library/layouts/Header/Header.tsx
+++ library/layouts/Header/Header.tsx
@@ -2,34 +2,13 @@
 
 import classNames from 'classnames';
 import Link from 'next/link';
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC } from 'react';
 import { Logo } from '@components/Logo/Logo';
 import styles from './Header.module.scss';
+import { useHeaderScroll } from './useHeaderScroll';
 
 export const Header: FC = () => {
-    const [showBar, setShowBar] = useState(false);
-
-    const calculate = useCallback(() => {
-        if (showBar) {
-            setShowBar(true);
-        } else {
-            setShowBar(window.scrollY > 10);
-        }
-    }, [showBar]);
-
-    useEffect(() => {
-        calculate();
-
-        window.addEventListener('scroll', () => {
-            calculate();
-        });
-
-        return () => {
-            window.removeEventListener('scroll', () => {
-                return;
-            });
-        };
-    }, [calculate]);
+    const { showBar } = useHeaderScroll();
 
     return (
         <header className={styles.header}>

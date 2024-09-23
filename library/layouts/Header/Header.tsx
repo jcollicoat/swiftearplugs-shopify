17b214eac27a4b0843515264e837a225b3c1ed29
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-23 13:03:42 +0100

    working cart modal in header

--- library/layouts/Header/Header.tsx
+++ library/layouts/Header/Header.tsx
@@ -2,13 +2,16 @@
 
 import classNames from 'classnames';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
+import { Icon } from '@components/Icon/Icon';
 import { Logo } from '@components/Logo/Logo';
 import styles from './Header.module.scss';
 import { useHeaderScroll } from './useHeaderScroll';
 
 export const Header: FC = () => {
     const { showBar } = useHeaderScroll();
+    const [cartOpen, setCartOpen] = useState(false);
+    const toggleCart = useCallback(() => setCartOpen(!cartOpen), [cartOpen]);
 
     return (
         <header className={styles.header}>
@@ -16,6 +19,18 @@ export const Header: FC = () => {
                 <Link href="/" className={styles.logo}>
                     <Logo />
                 </Link>
+                <button className={styles.cart} onClick={toggleCart}>
+                    <Icon icon="Cart" />
+                    Cart
+                </button>
+            </div>
+            <div
+                className={classNames(
+                    styles.modalBackground,
+                    cartOpen && styles.open,
+                )}
+            >
+                <div className={styles.modal}></div>
             </div>
         </header>
     );

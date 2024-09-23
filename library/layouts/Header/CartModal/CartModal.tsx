6551c87c0b8eb2170b3d9e9e4923e4e075e2000e
Joseph Collicoat <jcollicoat@gmail.com>
2024-09-23 14:24:01 +0100

    show line items in cart

--- library/layouts/Header/CartModal/CartModal.tsx
+++ library/layouts/Header/CartModal/CartModal.tsx
@@ -5,6 +5,7 @@ import { FC, useEffect } from 'react';
 import { createCartAndSetCookie } from '@shopify/cart/actions';
 import { useCart } from '@shopify/cart/cart-context';
 import styles from './CartModal.module.scss';
+import { Item } from './Item/Item';
 
 export const CartModal: FC = () => {
     const { cart, isCartOpen } = useCart();
@@ -15,11 +16,35 @@ export const CartModal: FC = () => {
         }
     }, [cart]);
 
+    if (!cart) return null;
+
+    const sortedItems = cart.lines.sort((a, b) => {
+        if (
+            a.merchandise.selectedOptions[0].value >
+            b.merchandise.selectedOptions[0].value
+        ) {
+            return 1;
+        }
+        if (
+            b.merchandise.selectedOptions[0].value >
+            a.merchandise.selectedOptions[0].value
+        ) {
+            return -1;
+        }
+        return 0;
+    });
+
     return (
         <div
             className={classNames(styles.background, isCartOpen && styles.open)}
         >
-            <div className={styles.modal}>[CART]</div>
+            <div className={styles.modal}>
+                <div className={styles.items}>
+                    {sortedItems.map((item) => (
+                        <Item key={item.id} item={item} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
